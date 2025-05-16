e2476ad86bebc8f7826220ea1a9adec44f1282f6
LucasMinervini38 <Lucas.Minervini@bizitglobalarg.onmicrosoft.com>
2025-05-16 10:17:49 -0300

    Estilos Hoja Excel

--- src/main/java/com/bot/telegramdocreader/service/ExportExcel.java
+++ src/main/java/com/bot/telegramdocreader/service/ExportExcel.java
@@ -37,7 +37,7 @@ public class ExportExcel {
         workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("Emisor");
 
-        // Crear estilo para encabezados
+        // Estilo para encabezados (azul oscuro, blanco, centrado, borde)
         CellStyle headerStyle = workbook.createCellStyle();
         Font headerFont = workbook.createFont();
         headerFont.setBold(true);
@@ -45,31 +45,37 @@ public class ExportExcel {
         headerStyle.setFont(headerFont);
         headerStyle.setFillForegroundColor(IndexedColors.DARK_BLUE.getIndex());
         headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        headerStyle.setAlignment(HorizontalAlignment.CENTER);
+        headerStyle.setBorderTop(BorderStyle.THIN);
+        headerStyle.setBorderBottom(BorderStyle.THIN);
+        headerStyle.setBorderLeft(BorderStyle.THIN);
+        headerStyle.setBorderRight(BorderStyle.THIN);
 
-        // Crear estilo para datos
+        // Estilo para datos (amarillo claro, borde, alineado)
         CellStyle dataStyle = workbook.createCellStyle();
         dataStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
         dataStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        dataStyle.setAlignment(HorizontalAlignment.CENTER);
+        dataStyle.setBorderTop(BorderStyle.THIN);
+        dataStyle.setBorderBottom(BorderStyle.THIN);
+        dataStyle.setBorderLeft(BorderStyle.THIN);
+        dataStyle.setBorderRight(BorderStyle.THIN);
 
-        // Encabezados
+        // Encabezados personalizados
+        String[] headers = new String[]{"Fecha", "Tipo Operación", "Cuit", "Monto Bruto", "Banco receptor"};
         Row headerRow = sheet.createRow(0);
-        String[] headers;
-        if ("PREX".equals(transferencia.getBank())) {
-            headers = new String[]{"Fecha", "Tipo de Operación", "Cuit", "Monto Bruto", "CBU/CVU Destino", "Cuenta Destino"};
-        } else {
-            headers = new String[]{"Fecha", "Tipo de Operación", "Cuit", "Monto Bruto", "Banco Receptor"};
-        }
-        
         for (int i = 0; i < headers.length; i++) {
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(headers[i]);
             cell.setCellStyle(headerStyle);
         }
 
+        // Fila de datos (ejemplo, puedes adaptar los campos según TransferDTO)
         Row dataRow = sheet.createRow(1);
         Cell dateCell = dataRow.createCell(0);
         dateCell.setCellValue(transferencia.getDate());
         dateCell.setCellStyle(dataStyle);
+
 
         Cell typeCell = dataRow.createCell(1);
         typeCell.setCellValue(transferencia.getTypeOFTransfer());
@@ -86,17 +92,7 @@ public class ExportExcel {
         Cell bankCell = dataRow.createCell(4);
         bankCell.setCellValue(transferencia.getBank());
         bankCell.setCellStyle(dataStyle);
-        
-        if ("PREX".equals(transferencia.getBank())) {
-            Cell cbuCell = dataRow.createCell(5);
-            cbuCell.setCellValue(transferencia.getCbuDestino());
-            cbuCell.setCellStyle(dataStyle);
 
-            Cell accountCell = dataRow.createCell(6);
-            accountCell.setCellValue(transferencia.getCuentaDestino());
-            accountCell.setCellStyle(dataStyle);
-        }
-        
         // Ajustar ancho de columnas
         for (int i = 0; i < headers.length; i++) {
             sheet.autoSizeColumn(i);
@@ -105,7 +101,7 @@ public class ExportExcel {
         // Generar nombre de archivo con timestamp
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
         fileName = System.getProperty("user.dir") + "/excelFolder/Transferencia_" + timestamp + ".xlsx";
-        
+
         // Crear directorio si no existe
         java.io.File directory = new java.io.File(System.getProperty("user.dir") + "/excelFolder");
         if (!directory.exists()) {
--- src/main/java/com/bot/telegramdocreader/service/TelegramFileService.java
+++ src/main/java/com/bot/telegramdocreader/service/TelegramFileService.java
@@ -66,69 +66,94 @@ public class TelegramFileService {
             return "Error: No hay transferencias para concatenar";
         }
 
-        // Obtener la lista de archivos Excel en la carpeta
         File folder = new File("excelFolder");
         File[] excelFiles = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".xlsx"));
 
         try (Workbook workbook = new XSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("Transferencias Concatenadas");
 
-            // Crear estilo para encabezados en negrita
+            // Estilo para encabezados (azul oscuro, blanco, centrado, borde)
             CellStyle headerStyle = workbook.createCellStyle();
             Font headerFont = workbook.createFont();
             headerFont.setBold(true);
+            headerFont.setColor(IndexedColors.WHITE.getIndex());
             headerStyle.setFont(headerFont);
+            headerStyle.setFillForegroundColor(IndexedColors.DARK_BLUE.getIndex());
+            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            headerStyle.setAlignment(HorizontalAlignment.CENTER);
+            headerStyle.setBorderTop(BorderStyle.THIN);
+            headerStyle.setBorderBottom(BorderStyle.THIN);
+            headerStyle.setBorderLeft(BorderStyle.THIN);
+            headerStyle.setBorderRight(BorderStyle.THIN);
 
-            // Crear encabezados
+            // Estilo para datos (amarillo claro, borde, alineado)
+            CellStyle dataStyle = workbook.createCellStyle();
+            dataStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+            dataStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            dataStyle.setAlignment(HorizontalAlignment.CENTER);
+            dataStyle.setBorderTop(BorderStyle.THIN);
+            dataStyle.setBorderBottom(BorderStyle.THIN);
+            dataStyle.setBorderLeft(BorderStyle.THIN);
+            dataStyle.setBorderRight(BorderStyle.THIN);
+
+            // Encabezados personalizados
+            String[] headers = new String[]{"Fecha", "Tipo Operación", "Cuit", "Monto Bruto", "Banco receptor"};
             Row headerRow = sheet.createRow(0);
-            Cell headerCell0 = headerRow.createCell(0);
-            headerCell0.setCellValue("Fecha");
-            headerCell0.setCellStyle(headerStyle);
-
-            Cell headerCell1 = headerRow.createCell(1);
-            headerCell1.setCellValue("Tipo de Operación");
-            headerCell1.setCellStyle(headerStyle);
-
-            Cell headerCell2 = headerRow.createCell(2);
-            headerCell2.setCellValue("CUIT");
-            headerCell2.setCellStyle(headerStyle);
-
-            Cell headerCell3 = headerRow.createCell(3);
-            headerCell3.setCellValue("Monto Bruto");
-            headerCell3.setCellStyle(headerStyle);
-
-            Cell headerCell4 = headerRow.createCell(4);
-            headerCell4.setCellValue("Banco Receptor");
-            headerCell4.setCellStyle(headerStyle);
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
+                cell.setCellStyle(headerStyle);
+            }
 
             // Agregar datos de todas las transferencias
             int rowNum = 1;
             for (TransferDTO transferencia : transferencias) {
                 Row row = sheet.createRow(rowNum++);
-                row.createCell(0).setCellValue(transferencia.getDate());
-                row.createCell(1).setCellValue(transferencia.getTypeOFTransfer());
-                row.createCell(2).setCellValue(transferencia.getCuit());
-                row.createCell(3).setCellValue(transferencia.getAmount());
-                row.createCell(4).setCellValue(transferencia.getBank());
+                Cell dateCell = row.createCell(0);
+                dateCell.setCellValue(transferencia.getDate());
+                dateCell.setCellStyle(dataStyle);
+
+               /*  Cell photoCell = row.createCell(1);
+                photoCell.setCellValue(transferencia.getPhotoName() != null ? transferencia.getPhotoName() : "");
+                photoCell.setCellStyle(dataStyle);
+                */
+
+                Cell typeCell = row.createCell(1);
+                typeCell.setCellValue(transferencia.getTypeOFTransfer());
+                typeCell.setCellStyle(dataStyle);
+
+                Cell cuitCell = row.createCell(2);
+                cuitCell.setCellValue(transferencia.getCuit());
+                cuitCell.setCellStyle(dataStyle);
+
+                Cell amountCell = row.createCell(3);
+                amountCell.setCellValue(transferencia.getAmount());
+                amountCell.setCellStyle(dataStyle);
+
+                Cell bankCell = row.createCell(4);
+                bankCell.setCellValue(transferencia.getBank());
+                bankCell.setCellStyle(dataStyle);
+
+               /* Cell statusCell = row.createCell(6);
+                statusCell.setCellValue(transferencia.getStatusOp() != null ? transferencia.getStatusOp() : "");
+                statusCell.setCellStyle(dataStyle);
+                */ 
             }
 
             // Ajustar el ancho de las columnas
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < headers.length; i++) {
                 sheet.autoSizeColumn(i);
             }
 
-            // Guardar el archivo
             String timeStap = String.valueOf(System.currentTimeMillis());
-            String fileName = EXCEL_FOLDER + "transferencias_concatenadas"+ timeStap + ".xlsx";
+            String fileName = EXCEL_FOLDER + "transferencias_concatenadas" + timeStap + ".xlsx";
             java.io.FileOutputStream fileOut = new java.io.FileOutputStream(fileName);
             workbook.write(fileOut);
             fileOut.close();
 
-            // Eliminar los archivos Excel individuales
             if (excelFiles != null) {
                 for (File excelFile : excelFiles) {
-                    if (excelFile.delete()) {
-                    } else {
+                    if (!excelFile.delete()) {
                         System.out.println("No se pudo eliminar el archivo: " + excelFile.getName());
                     }
                 }
